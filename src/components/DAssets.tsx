8976370d30bf993135cfd7d6a0c03c5cc2ca7ef3
Akriti <akritikohli05@gmail.com>
2024-09-13 16:10:37 +0530

    set up assets and statutory

--- src/components/DAssets.tsx
+++ src/components/DAssets.tsx
@@ -1,8 +1,26 @@
 
 const DAssets:React.FC = () => {
   return (
-    <div>
-      Assests
+    <div className="assets-wrapper">
+      <div className="assets-heading">
+        <div>#</div>
+        <div>Name</div>
+        <div>Asset ID</div>
+        <div>Assigned Date</div>
+        <div>Assignee</div>
+      </div>
+
+      <div className="assets-compoponent-container">
+        <div className="assets-component">
+          <div>1</div>
+          <div className="name">Laptop</div>
+          <div>AST-001</div>
+          <div>12Nov,2024 10:32AM</div>
+          <div className="assignee">John Paul Raj</div>
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
--- src/components/DStatutory.tsx
+++ src/components/DStatutory.tsx
@@ -2,8 +2,187 @@
 
 const DStatutory:React.FC = () => {
   return (
-    <div>
-      Statutory
+    <div className="statutory-wrapper">
+      
+      <div className="salary-info-container">
+        <h2>Basic Salary Information</h2>
+        <div className="salary-info">
+          <div>
+            <p>Salary basis</p>
+            <select name="salary-basis" id="salary">
+              <option value="option1">Select salary basis type</option>
+              <option value="option2">Hourly</option>
+              <option value="option3">Daily</option>
+              <option value="option4">Weekly</option>
+              <option value="option5">Monthly</option>
+            </select>
+          </div>
+          <div>
+            <p>Salary amount</p>
+            <input type="text" placeholder="0.00" />
+          </div>
+          <div>
+            <p>Payment type</p>
+            <select name="salary-basis" id="salary">
+              <option value="option1">Select payment type</option>
+              <option value="option1">Bank transfer</option>
+              <option value="option1">Check</option>
+              <option value="option1">Cash</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="PF-info-container">
+        <h2>PF Information</h2>
+        <div className="PF-info"> 
+          <div>
+            <div>
+              <p>PF contribution</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select PF contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+            <div>
+              <p>PF No.</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select PF contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Employee PF rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select PF contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+            <div>
+              <p>Additional rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select additional rate </option>
+                <option value="option2">0%</option>
+                <option value="option3">1%</option>
+                <option value="option4">2%</option>
+                <option value="option5">3%</option>
+                <option value="option6">4%</option>
+                <option value="option7">5%</option>
+                <option value="option8">6%</option>
+                <option value="option9">7%</option>
+                <option value="option10">8%</option>
+                <option value="option11">9%</option>
+                <option value="option12">10%</option>
+              </select>
+            </div>
+            <div>
+              <p>Total rate</p>
+              <input type="text" placeholder="11%" />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Employee PF rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select PF contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+            <div>
+              <p>Additional rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select additional rate </option>
+                <option value="option2">0%</option>
+                <option value="option3">1%</option>
+                <option value="option4">2%</option>
+                <option value="option5">3%</option>
+                <option value="option6">4%</option>
+                <option value="option7">5%</option>
+                <option value="option8">6%</option>
+                <option value="option9">7%</option>
+                <option value="option10">8%</option>
+                <option value="option11">9%</option>
+                <option value="option12">10%</option>
+              </select>
+            </div>
+            <div>
+              <p>Total rate</p>
+              <input type="text" placeholder="11%" />
+            </div>
+          </div>
+          </div>
+      </div>
+
+      <div className="ESI-info-container">
+        <h2>ESI Information</h2>
+
+        <div className="ESI-info">
+          <div>
+            <div>
+              <p>ESI contribution</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select ESI contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+            <div>
+              <p>ESI No.</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select ESI contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Employee ESI rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select ESI contribution </option>
+                <option value="option1">Yes</option>
+                <option value="option1">No</option>
+              </select>
+            </div>
+            <div>
+              <p>Additional rate</p>
+              <select name="pf-contribution" id="pf">
+                <option value="option1">Select additional rate </option>
+                <option value="option2">0%</option>
+                <option value="option3">1%</option>
+                <option value="option4">2%</option>
+                <option value="option5">3%</option>
+                <option value="option6">4%</option>
+                <option value="option7">5%</option>
+                <option value="option8">6%</option>
+                <option value="option9">7%</option>
+                <option value="option10">8%</option>
+                <option value="option11">9%</option>
+                <option value="option12">10%</option>
+              </select>
+            </div>
+            <div>
+              <p>Total rate</p>
+              <input type="text" placeholder="11%" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="save">
+        <button>
+          Save
+        </button>
+      </div>
     </div>
   )
 }
